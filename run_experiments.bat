cb64982c93c5e180fde940ba64cb9c12864732b7
Luca <lucardito03@gmail.com>
2025-04-08 10:13:09 +0200

    new results

--- run_experiments.bat
+++ run_experiments.bat
@@ -1,23 +1,20 @@
 @echo off
 echo Inizio esecuzione esperimenti...
 
-echo Esecuzione con config: 128_03_07.yaml
-python main.py --config config\128_03_07.yaml
+echo Esecuzione con config: config\interpolate\32x32_256_03_07.yaml
+python main.py --config config\interpolate\32x32_256_03_07.yaml
 
-echo Esecuzione con config: 128_04_06.yaml
-python main.py --config config\128_04_06.yaml
+echo Esecuzione con config: config\interpolate\64x64_256_03_07.yaml
+python main.py --config config\interpolate\64x64_256_03_07.yaml
 
-echo Esecuzione con config: 256_03_07.yaml
-python main.py --config config\256_03_07.yaml
+echo Esecuzione con config: config\no_resize\5x5_256_02_08.yaml
+python main.py --config config\no_resize\5x5_256_02_08.yaml
 
-echo Esecuzione con config: 256_04_06.yaml
-python main.py --config config\256_04_06.yaml
+echo Esecuzione con config: config\interpolate\32x32_256_04_06.yaml
+python main.py --config config\interpolate\32x32_256_04_06.yaml
 
-echo Esecuzione con config: 512_03_07.yaml
-python main.py --config config\512_03_07.yaml
-
-echo Esecuzione con config: 512_04_06.yaml
-python main.py --config config\512_04_06.yaml
+echo Esecuzione con config: config\interpolate\64x64_256_04_06.yaml
+python main.py --config config\interpolate\64x64_256_04_06.yaml
 
 echo Tutti gli esperimenti sono stati completati.
 pause
