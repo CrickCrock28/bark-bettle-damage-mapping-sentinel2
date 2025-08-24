bd0924424f17511eecee8931a42d8f20c77e3335
CrickCrock28 <lucardito03@gmail.com>
2025-08-24 17:42:06 +0200

    final commit

--- scripts/run_tests.bat
+++ scripts/run_tests.bat
@@ -12,4 +12,40 @@ python main.py --config config\tests\all_data_128_05_05.yaml --test
 echo Esecuzione con config: config\tests\all_data_256_05_05.yaml
 python main.py --config config\tests\all_data_256_05_05.yaml --test
 
+echo Esecuzione con config: config\experiments\filtered_128_01_09.yaml
+python main.py --config config\experiments\filtered_128_01_09.yaml --test
+
+echo Esecuzione con config: config\experiments\filtered_256_01_09.yaml
+python main.py --config config\experiments\filtered_256_01_09.yaml --test
+
+echo Esecuzione con config: config\experiments\all_data_128_02_08.yaml
+python main.py --config config\experiments\all_data_128_02_08.yaml --test
+
+echo Esecuzione con config: config\experiments\all_data_256_02_08.yaml
+python main.py --config config\experiments\all_data_256_02_08.yaml --test
+
+echo Esecuzione con config: config\experiments\filtered_128_03_07.yaml
+python main.py --config config\experiments\filtered_128_03_07.yaml --test
+
+echo Esecuzione con config: config\experiments\filtered_256_03_07.yaml
+python main.py --config config\experiments\filtered_256_03_07.yaml --test
+
+echo Esecuzione con config: config\experiments\all_data_128_04_06.yaml
+python main.py --config config\experiments\all_data_128_04_06.yaml --test
+
+echo Esecuzione con config: config\experiments\all_data_256_04_06.yaml
+python main.py --config config\experiments\all_data_256_04_06.yaml --test
+
+echo Esecuzione con config: config\experiments\filtered_128_05_05.yaml
+python main.py --config config\experiments\filtered_128_05_05.yaml --test
+
+echo Esecuzione con config: config\experiments\filtered_256_05_05.yaml
+python main.py --config config\experiments\filtered_256_05_05.yaml --test
+
+echo Esecuzione con config: config\experiments\all_data_128_01_09.yaml
+python main.py --config config\experiments\all_data_128_01_09.yaml --test
+
+echo Esecuzione con config: config\experiments\all_data_256_01_09.yaml
+python main.py --config config\experiments\all_data_256_01_09.yaml --test
+
 pause
